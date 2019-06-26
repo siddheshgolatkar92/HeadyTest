a768c2844547d24d5986bc4833c28ad64910c08f
siddhesh <siddheshgolatkar.insomniacs@gmail.com>
2019-06-26 12:21:16 +0530

    Initial commit

---
+++ app/src/main/java/com/headytest/siddhesh/headytest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.headytest.siddhesh.headytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
